89125f1ac063d9020102f4c4ab41bae746f39a2a
Pablo Perez <pppablo83@gmail.com>
2017-03-26 18:22:14 +0200

    Added button to Home page to login

--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { Jumbotron } from 'react-bootstrap'
+import { Jumbotron , Button } from 'react-bootstrap'
 
 const HomePage = () => (
   <Jumbotron>
     <h1>Welcome to Gym Tracker</h1>
     <p>This is a simple app to log your gym workouts</p>
+    <p><Button bsStyle="primary">Login to start!</Button></p>
   </Jumbotron>
 )
 
